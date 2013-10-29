50ff7204f91f911a2d7dd0c012e338bcd3a8ed40
JT <jt@zoovy.com>
2013-10-28 20:53:41 -0700

    updated anycb and anytabs

--- anytabs.js
+++ anytabs.js
@@ -6,23 +6,23 @@
 		_init : function(){
 			var self = this,
 			o = self.options, //shortcut
-			$t = self.element; //this is the targeted element (ex: $('#bob').anymessage() then $t is bob)
-			
-			if($t.attr('widget') == 'anytabs')	{} //id has already been set as tabs.
+			$t = self.element; //this is the targeted element (ex: $('#bob').anytabs() then $t is bob)
+
+			if($t.attr('data-widget-anytabs'))	{
+				app.u.dump("data-widget-anytabs -> already enabled.");
+				} //element has already been set as tabs.
 			else	{
-				console.log('got into init else');
-				$t.attr('widget','anytabs')
+				$t.attr('data-widget-anytabs',true)
 				$t.addClass('ui-tabs ui-widget ui-widget-anytabs')
 				self.tabs = $("ul",$t).first();
-	
-	//style and move tabs into container.
+//style and move tabs into container.
 				self._handleContent();
 				self._addClasses2Content();
 				
-	//style and add click events to tabs.
+//style and add click events to tabs.
 				self._addClasses2Tabs();
 				self._addEvent2Tabs();
-	//make a tab visible/active.
+//make a tab visible/active.
 				self._handleDefaultTab();
 				}
 			}, //_init
@@ -55,17 +55,25 @@
 
 		_addEvent2Tabs : function()	{
 			var self = this;
-			this.tabs.find('li').each(function(){
-				$(this).off('click.anytab').on('click.anytab',function(){
-					self.reveal($(this));
-					});
+// *** 201336 -> tab clicks now use delegated events. more efficient.
+			this.tabs.on('click','a',function(event){
+				event.preventDefault();
+				self.reveal($(this).parent());
+//				if($(this).data('app-click'))	{
+//					app.u.executeEvent($(this),{'type':'click'});
+//					}
+				return false;
 				});
 			},
 
 		_addClasses2Tabs : function()	{
 			this.tabs.addClass('ui-tabs-nav ui-helper-reset ui-helper-clearfix').css({'padding-left':'0px'});
 			this.tabs.find('a').addClass('ui-tabs-anchor').attr('role','presentation');
-			this.tabs.find('li').addClass('ui-state-default ui-corner-top');
+// * 201336 -> wanted a data reference on the li of the tab that was consistent. can be used to show or hide tab, if needed.
+			this.tabs.find('li').each(function(){
+				$(this).addClass('ui-state-default ui-corner-top');
+				$(this).attr('data-anytabs-tab',$(this).find('a').first().attr('href').substr(1));
+				});
 			},
 //create a container div and add each content panel to it.
 		_handleContent : function()	{
@@ -81,28 +89,30 @@
 
 		_addClasses2Content : function()	{
 			$("[data-anytab-content]",this.element).addClass("ui-tabs-panel ui-widget-content ui-corner-bottom").css('display','none');
-			
 			},
 
-		
-
 		reveal : function($tab)	{
+			var $newTab = false;
+			//method accepts string or jquery object as the trigger.
 			if(typeof $tab == 'string')	{
-				if($tab.charAt(0) == '#')	{}
-				else	{$tab = '#'+$tab}
-				$('a',this.element).each(function(){
-					if($(this).attr('href') == $tab)	{
-						$(this).trigger('click'); //will re-execute this function with $tab as object.
-						return false; //breaks out of each loop.
-						}
-					});
+				var $href = $("a[href='"+($tab.charAt(0) == '#' ? $tab : '#'+$tab)+"']",this.tabs);
+				if($href.length)	{
+					$newTab = $href.parent();
+					delete $href;
+					}
+				else	{}
+				}
+			else if($tab instanceof jQuery)	{
+				$newTab = $tab;
+				}
+			else	{
 				
 				}
-			else if(typeof $tab == 'object')	{
-				var dac = $tab.find('a').attr('href').substring(1); //data-anytab-content
-				document.location.hash = dac; //set hash. triggering click doesn't do this.
+			
+			if($newTab)	{
+				var dac = $newTab.find('a').attr('href').substring(1); //data-anytab-content
 				this.tabs.find('.ui-state-active').removeClass('ui-state-active ui-tabs-active');
-				$tab.addClass('ui-state-active ui-tabs-active');
+				$newTab.addClass('ui-state-active ui-tabs-active');
 
 				this.tabContent.find('.ui-tabs-panel').hide();
 				$("[data-anytab-content='"+dac+"']",this.tabContent).show();
@@ -112,7 +122,12 @@
 
 //clear the message entirely. run after a close. removes element from DOM.
 		destroy : function(){
-			this.element.empty().remove();
+			this.element.intervaledEmpty(500,true);
+			this.element.removeClass("ui-tabs");
+			this.element.removeClass("ui-widget");
+			this.element.removeClass("ui-widget-anytabs");
+			this.element.data("widget-anytabs","");
+			this.element.attr("data-widget-anytabs","").removeAttr('data-widget-anytabs');
 			}
 		}); // create the widget
 })(jQuery); 
